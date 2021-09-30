d17a909cc5ab5fcc6215e9d57d0098f022eb2967
BryanMartinezUMG28 <bmartinezg8@miumg.edu.gt>
2021-09-29 23:32:28 -0600

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.plantilla')
+
+@section('content')
+    <br>
+    <div class="card">
+        <div class="card-header">
+            TITULO DE CONTENIDO DE PLANTILLA
+        </div>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>Contenido del curso - plantilla de blade</p>
+
+            </blockquote>
+        </div>
+    </div>
+@endsection
